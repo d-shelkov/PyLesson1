a9e62ee136cf0d90b9235575d2b7c62120fe6f08
Tsar, just Tsar <dmitry.shelkov@gmail.com>
2023-06-29 00:52:24 +0800

    Lucky ticket number

--- PyLesson1Task6.py
+++ PyLesson1Task6.py
@@ -1,12 +1,12 @@
 RunAgain = True
 second = False
-papcrnum = '0' # Paper crane(s) number
-num = 0
+ticketnum = '0' # Ticket number
 while RunAgain:
-    if second: print("You need to enter exactly a positive sixfold number. Please be attentive")
-    papcrnum = input("Please enter a positive sixfold number: ")
-    if papcrnum.isdigit(): 
-        num = int(papcrnum)
-        RunAgain = False if num % 6 == 0 and num > 0 else True
+    if second: print("You need to enter exactly a six digit positive ticket number. Please be attentive")
+    ticketnum = input("Please enter a six digit positive ticket number: ")
+    if ticketnum.isdigit(): 
+        RunAgain = False if 99999 < int(ticketnum) < 1000000 else True
     second = True
-print('Peter and Sergey made the ', int(num/6) ,' of paper crane(s) each. And Kate made ', int(num/6*4),' of paper crane(s).')
+if int(ticketnum[0])+int(ticketnum[1])+int(ticketnum[2]) == int(ticketnum[3])+int(ticketnum[4])+int(ticketnum[5]):
+    print("Congratulations!!! You have lucky tycket.")
+else: print("Unfortunatly you don't have lucky ticket now. Probably you will be lucky next time!")
